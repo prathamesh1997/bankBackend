645d1295422212305f401ab3c8b577a8dab28635
prathamesh <orgzapp.prathamesh@gmail.com>
2019-08-24 10:51:54 +0530

    execute query

--- module/executeQuery.js
+++ module/executeQuery.js
@@ -2,24 +2,48 @@ const sql = require('mssql');
 
 const DATABASE_CONFIG = require('../config');
 
+
+
 var ExecuteQuery =  function(response,query){
-    sql.connect(DATABASE_CONFIG,function(err){
-        if(err){
-            console.log(`Error while connecting database ${err}`)
-        }else{
-            //Create Request Object
-            var request = new sql.Request();
-
+    const pool = new sql.ConnectionPool(DATABASE_CONFIG)
+    const connection = pool;
+    
+    connection.connect()
+        .then(()=>{
+            
+            //create database request
+            var request = new sql.Request(connection);
+            
             //query to database
-
             request.query(query,function(err,res){
                 if(err){
                     console.log(`Error while querying database ${err}`)
                 }else{
-                    response.send(res)
+                    return response.send(res.recordset)
                 }
             })
-        }
-    })
+        })  
+        .catch(err=>{
+            console.log(err)
+        })
+    // sql.connect(DATABASE_CONFIG,function(err){
+    //     if(err){
+    //         console.log(`Error while connecting database ${err}`)
+    //     }else{
+    //          //Create Request Object
+    //           var request = new sql.Request();
+
+    //         //query to database
+
+    //         request.query(query,function(err,res){
+    //             if(err){
+    //                 console.log(`Error while querying database ${err}`)
+    //             }else{
+    //                 console.log(res.recordset)
+    //                return response.send(res.recordset)
+    //             }
+    //         })
+    //     }
+    // })
 }
 module.exports = ExecuteQuery;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const bodyparser = require('body-parser');
+
 const ExecuteQuery = require('./module/executeQuery');
 
 const app = express();
@@ -9,11 +11,28 @@ const SELECT_ALL_USERS = 'SELECT * FROM users';
 
 //MIDDLEWARE TO HANDLE REQUEST'S
 app.use(cors());
+app.use(bodyparser.urlencoded({extended:false}))
+app.use(bodyparser.json())
 
 //API'S 
-
+// ***********************
+//Get all user details
 app.get('/api/users',(req,res)=>{
     ExecuteQuery(res,SELECT_ALL_USERS);
+})
+
+//get user details by id
+
+app.get('/api/customer/:id',(req,res)=>{
+    ExecuteQuery(res,`SELECT * FROM users where id=${req.params.id}`);
+})
+
+//verify details
+
+app.post('/api/verifyUser',(req,res)=>{
+    var id = req.body.id;
+    var verified ='verified';
+    ExecuteQuery(res,"update users set status = 'verified' where id = '"+id+"'");
 })
 
 app.get('/userDetails',(req,res)=>{
